571bb78f8ed063079e3b3bee4c5d8521dfcbf1f9
mnova <mnova@atex.com>
2015-06-19 15:50:19 +0200

    updated the js to match the new resources

--- src/main/javascript/newsstand.js
+++ src/main/javascript/newsstand.js
@@ -21,6 +21,15 @@ Atex.namespace("Atex.plugin.newsstand");
           }, options));
         }
 
+        function ajaxJson(method, ajaxUrl, options) {
+          api.log('ajaxjson m:' + method + ' u:' + ajaxUrl);
+          return $.ajax($.extend({
+            type: method,
+            url: ajaxUrl,
+            dataType: 'json'
+          }, options));
+        }
+
         function ajaxPost(ajaxUrl, options) {
           return ajax('POST', ajaxUrl, options);
         }
@@ -29,15 +38,51 @@ Atex.namespace("Atex.plugin.newsstand");
           return ajax('GET', ajaxUrl, options);
         }
 
+        function ajaxJPost(ajaxUrl, options) {
+          return ajaxJson('POST', ajaxUrl, options);
+        }
+
+        function ajaxJGet(ajaxUrl, options) {
+          return ajaxJson('GET', ajaxUrl, options);
+        }
+
         function login(catalog, issueCode) {
           api.log('login c:' + catalog + ' i:' + issueCode);
           var url = '/newsstandws/issue/' + issueCode + '/login?catalog=' + encodeURIComponent(catalog);
           return ajaxPost(url);
         };
 
+        function getCatalog(catalog) {
+          api.log('catalog c:' + catalog);
+          var url = '/newsstandws/catalog/' + catalog;
+          return ajaxJGet(url);
+        };
+
+        function getPublication(publicationId) {
+          api.log('publication id:' + publicationId);
+          var url = '/newsstandws/publication/' + publicationId;
+          return ajaxJGet(url);
+        };
+
+        function getIssue(issueCode) {
+          api.log('issue code:' + issueCode);
+          var url = '/newsstandws/issue/' + issueCode;
+          return ajaxJGet(url);
+        };
+
+        function getPublicationFromIssue(catalog, issueCode) {
+          api.log('publication c:' + catalog + ' i:' + issueCode);
+          var url = '/newsstandws/catalog/' + catalog + '/publication?issueCode=' + encodeURIComponent(issueCode);
+          return ajaxJGet(url);
+        };
+
         // Public interface
         return {
-            login: login
+            login: login,
+            getCatalog: getCatalog,
+            getPublication: getPublication,
+            getIssue: getIssue,
+            getPublicationFromIssue: getPublicationFromIssue
         };
     }());
 }(this));
